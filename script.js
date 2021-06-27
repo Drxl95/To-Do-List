a159fdd737ed89320b34b42b804a4bdbd91bd66c
Drxl95 <uebelhorr@gmail.com>
2021-06-26 20:48:49 -0500

    made app

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function newItem() {
+    let li = $('<li></li>');
+    $('#list').append(li);
+
+    let inputValue = $('input').val();
+
+    let text = $(document.createTextNode(inputValue));
+    li.append(text);
+
+    if(inputValue === "") {
+        alert('You must write something!');
+    }else {
+        let list = $('#list');
+        list.append(li);
+    }
+
+    function crossOut() {
+        li.toggleClass("strike");
+    }
+
+    li.on('dblclick', crossOut);
+    
+    
+    let crossOutButton = $('<crossoutButton></crossoutButton>');
+    li.append(crossOutButton);
+
+    crossOutButton.append(document.createTextNode('X'));  
+    li.append(crossOutButton);
+
+    function deleteListItem() {
+        li.addClass('delete')
+    }
+
+    crossOutButton.on('click',  deleteListItem );
+
+
+    $('#list').sortable();
+
+};
+
+
+
+
